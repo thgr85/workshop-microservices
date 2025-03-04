fc65c7ba69f9e5ecf3fe03f57f6403c93fe382c1
Thiago Oliveira <thgr85@outlook.com>
2025-03-04 12:03:31 -0300

    Projeto criado

---
+++ src/main/java/br/com/workshopspring/courseErudio/CourseErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.workshopspring.courseErudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseErudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/workshopspring/courseErudio/CourseErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.workshopspring.courseErudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
