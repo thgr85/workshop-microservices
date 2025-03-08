4155362fbc521807f22882fe416d58a765015646
Thiago Oliveira <thgr85@outlook.com>
2025-03-08 15:14:26 -0300

    FindAll, Create, Update and Delete to entity Person

--- src/main/java/br/com/workshopspring/courseErudio/controllers/PersonController.java
+++ src/main/java/br/com/workshopspring/courseErudio/controllers/PersonController.java
@@ -5,6 +5,8 @@ import br.com.workshopspring.courseErudio.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -16,7 +18,27 @@ public class PersonController {
     private PersonService personService;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable(value = "id") String id) throws Exception {
+    public Person findById(@PathVariable(value = "id") String id) {
         return personService.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return personService.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return personService.create(person);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return personService.update(person);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable(value = "id") String id) {
+        personService.delete(id);
     }
 }
--- src/main/java/br/com/workshopspring/courseErudio/services/PersonService.java
+++ src/main/java/br/com/workshopspring/courseErudio/services/PersonService.java
@@ -2,6 +2,9 @@ package br.com.workshopspring.courseErudio.services;
 
 import br.com.workshopspring.courseErudio.models.Person;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -19,5 +22,39 @@ public class PersonService {
         person.setAddress("Rua 123");
         person.setGender("Male");
         return person;
+    }
+
+    public List<Person> findAll() {
+        logger.info("Finding all persons...");
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Romeo - " + i);
+        person.setLastName("Gomes - " + i);
+        person.setAddress("Rua 123 - " + i);
+        person.setGender("Male - " + i);
+        return person;
+    }
+
+    public Person create(Person person) {
+        logger.info("Creating one person...");
+        return person;
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one person...");
+        return person;
+    }
+
+    public void delete(String id) {
+        logger.info("Deleting one person...");
     }
 }
